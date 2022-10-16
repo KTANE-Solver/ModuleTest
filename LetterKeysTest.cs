ec4f5f495d026eb445c6a595dd9f6d17f37896be
Nya <nyabentley@gmail.com>
2022-10-15 21:55:02 -0400

    Finihsed Letter Keys tests

--- LetterKeysTest.cs
+++ LetterKeysTest.cs
@@ -9,33 +9,35 @@ namespace ModuleTests
     [TestClass]
     public class LetterKeysTest
     {
+        //https://ktane.timwi.de/More/Logfile%20Analyzer.html#file=e8b692c145c1736e69223ccbca86ac9edeaac6d7;bomb=IE9JV0
+
         StreamWriter streamWriter = new StreamWriter("dummy.txt");
 
+        Bomb bomb = new Bomb(Day.Sunday, "IE9JV0", 0, 0, new Indicator("BOB", false, false), new Indicator("CAR", false, false),
+        new Indicator("CLR", false, false), new Indicator("FRK", false, false), new Indicator("FRQ", false, false), new Indicator("IND", false, false), new Indicator("MSA", false, false), new Indicator("NSA", false, false),
+        new Indicator("SIG", false, false), new Indicator("SND", false, false), new Indicator("TRN", false, false), false, 3, new Port("DVID", 3), new Port("Parallel", 0),
+        new Port("ps", 2), new Port("rj", 1), new Port("serial", 0), new Port("setero", 2));
+        
         [TestMethod]
         public void TestMethod1()
         {
-            LetterKeys module = new LetterKeys(null, streamWriter, 69);
-            Assert.AreEqual(module.SolveDebug(), "D");
+            LetterKeys module = new LetterKeys(bomb, streamWriter, 34);
+            Assert.AreEqual(module.SolveDebug(), "B");
             streamWriter.Close();
         }
 
         [TestMethod]
         public void TestMethod2()
         {
-            LetterKeys module = new LetterKeys(null, streamWriter, 6);
-            Assert.AreEqual(module.SolveDebug(), "A");
+            LetterKeys module = new LetterKeys(bomb, streamWriter, 41);
+            Assert.AreEqual(module.SolveDebug(), "B");
             streamWriter.Close();
         }
 
         [TestMethod]
         public void TestMethod3()
         {
-            Bomb bomb = new Bomb(Day.Sunday, "IIIII", 2, 2, new Indicator("BOB", false, false), new Indicator("CAR", false, false),
-                   new Indicator("CLR", false, false), new Indicator("FRK", true, true), new Indicator("FRQ", false, false), new Indicator("IND", false, false), new Indicator("MSA", false, false), new Indicator("NSA", false, false),
-                   new Indicator("SIG", false, false), new Indicator("SND", false, false), new Indicator("TRN", true, true), false, 2, new Port("DVID", 0), new Port("Parallel", 1),
-                   new Port("ps", 0), new Port("rj", 0), new Port("serial", 0), new Port("setero", 0));
-
-            LetterKeys module = new LetterKeys(bomb, streamWriter, 3);
+            LetterKeys module = new LetterKeys(bomb, streamWriter, 76);
             Assert.AreEqual(module.SolveDebug(), "B");
             streamWriter.Close();
         }
@@ -43,12 +45,7 @@ namespace ModuleTests
         [TestMethod]
         public void TestMethod4()
         {
-            Bomb bomb = new Bomb(Day.Sunday, "C", 1, 2, new Indicator("BOB", false, false), new Indicator("CAR", false, false),
-                   new Indicator("CLR", false, false), new Indicator("FRK", true, true), new Indicator("FRQ", false, false), new Indicator("IND", false, false), new Indicator("MSA", false, false), new Indicator("NSA", false, false),
-                   new Indicator("SIG", false, false), new Indicator("SND", false, false), new Indicator("TRN", true, true), false, 2, new Port("DVID", 0), new Port("Parallel", 1),
-                   new Port("ps", 0), new Port("rj", 0), new Port("serial", 0), new Port("setero", 0));
-
-            LetterKeys module = new LetterKeys(bomb, streamWriter, 22);
+            LetterKeys module = new LetterKeys(bomb, streamWriter, 26);
             Assert.AreEqual(module.SolveDebug(), "B");
             streamWriter.Close();
         }
@@ -56,40 +53,10 @@ namespace ModuleTests
         [TestMethod]
         public void TestMethod5()
         {
-            Bomb bomb = new Bomb(Day.Sunday, "C", 1, 2, new Indicator("BOB", false, false), new Indicator("CAR", false, false),
-                   new Indicator("CLR", false, false), new Indicator("FRK", true, true), new Indicator("FRQ", false, false), new Indicator("IND", false, false), new Indicator("MSA", false, false), new Indicator("NSA", false, false),
-                   new Indicator("SIG", false, false), new Indicator("SND", false, false), new Indicator("TRN", true, true), false, 2, new Port("DVID", 0), new Port("Parallel", 1),
-                   new Port("ps", 0), new Port("rj", 0), new Port("serial", 0), new Port("setero", 0));
-
-            LetterKeys module = new LetterKeys(bomb, streamWriter, 1);
+            LetterKeys module = new LetterKeys(bomb, streamWriter, 92);
             Assert.AreEqual(module.SolveDebug(), "C");
             streamWriter.Close();
         }
 
-        [TestMethod]
-        public void TestMethod6()
-        {
-            Bomb bomb = new Bomb(Day.Sunday, "A", 1, 2, new Indicator("BOB", false, false), new Indicator("CAR", false, false),
-                   new Indicator("CLR", false, false), new Indicator("FRK", true, true), new Indicator("FRQ", false, false), new Indicator("IND", false, false), new Indicator("MSA", false, false), new Indicator("NSA", false, false),
-                   new Indicator("SIG", false, false), new Indicator("SND", false, false), new Indicator("TRN", true, true), false, 2, new Port("DVID", 0), new Port("Parallel", 1),
-                   new Port("ps", 0), new Port("rj", 0), new Port("serial", 0), new Port("setero", 0));
-
-            LetterKeys module = new LetterKeys(bomb, streamWriter, 44);
-            Assert.AreEqual(module.SolveDebug(), "D");
-            streamWriter.Close();
-        }
-
-        [TestMethod]
-        public void TestMethod7()
-        {
-            Bomb bomb = new Bomb(Day.Sunday, "A", 1, 2, new Indicator("BOB", false, false), new Indicator("CAR", false, false),
-                   new Indicator("CLR", false, false), new Indicator("FRK", true, true), new Indicator("FRQ", false, false), new Indicator("IND", false, false), new Indicator("MSA", false, false), new Indicator("NSA", false, false),
-                   new Indicator("SIG", false, false), new Indicator("SND", false, false), new Indicator("TRN", true, true), false, 2, new Port("DVID", 0), new Port("Parallel", 1),
-                   new Port("ps", 0), new Port("rj", 0), new Port("serial", 0), new Port("setero", 0));
-
-            LetterKeys module = new LetterKeys(bomb, streamWriter, 46);
-            Assert.AreEqual(module.SolveDebug(), "A");
-            streamWriter.Close();
-        }
     }
 }
