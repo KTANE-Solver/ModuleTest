eaa5d92505bb83a377dc13333d57c1f80b7f0676
Nya <nyabentley@gmail.com>
2022-12-21 18:20:44 -0500

    Created binary puzzle tests

--- BinaryPuzzleTest.cs
+++ BinaryPuzzleTest.cs
@@ -1,17 +1,214 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
-using System.IO;
+using System.Drawing;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using New_KTANE_Solver;
 using System.Windows.Forms;
-using System.Collections.Generic;
 
-namespace ModuleTests
+namespace ModuleTest
 {
+    //https://ktane.timwi.de/lfa#file=1c5e8afec1479c495dee626a21f040d55766ce95
+
+    
+
     [TestClass]
     public class BinaryPuzzleTest
     {
-        //https://ktane.timwi.de/More/Logfile%20Analyzer.html#file=e0917707c1a8ff0c3a39e35cf8a6947a2e97a676;bomb=6P0NB6
+        //https://ktane.timwi.de/lfa#file=41f32e4d452e1bcd1e771a63647a73417e44d6c3
 
-        StreamWriter streamWriter = new StreamWriter("dummy.txt");
+        StreamWriter io = new StreamWriter("dummy.txt");
+
+        [TestMethod]
+        public void Test1()
+        {
+            char[,] grid = new char[,]
+                {
+                    { '-', '-', '0', '-', '1', '-' },
+                    { '1', '-', '0', '0', '-', '-' },
+                    { '-', '-', '-', '1', '-', '-' },
+                    { '-', '-', '-', '-', '-', '-' },
+                    { '0', '-', '-', '-', '1', '-' },
+                    { '-', '0', '-', '-', '-', '-' }
+
+                };
+
+
+            char[,] answer = new char[,]
+                {
+                    { '0', '1', '0', '0', '1', '1' },
+                    { '1', '1', '0', '0', '1', '0' },
+                    { '0', '0', '1', '1', '0', '1' },
+                    { '1', '1', '0', '1', '0', '0' },
+                    { '0', '0', '1', '0', '1', '1' },
+                    { '1', '0', '1', '1', '0', '0' }
+
+                };
+
+            BinaryPuzzle module = new BinaryPuzzle(grid, io);
+
+            char[,] output = module.Solve();
+
+            Assert.IsTrue(SameGrid(answer, output));
+
+            io.Close();
+        }
+
+        [TestMethod]
+        public void Test2()
+        {
+            char[,] grid = new char[,]
+                {
+                    { '1', '-', '-', '-', '-', '-' },
+                    { '1', '-', '0', '-', '-', '-' },
+                    { '-', '-', '1', '-', '1', '-' },
+                    { '-', '0', '-', '-', '1', '-' },
+                    { '-', '-', '-', '-', '-', '-' },
+                    { '-', '-', '1', '-', '1', '-' }
+
+                };
+
+
+            char[,] answer = new char[,]
+                {
+                    { '1', '0', '1', '0', '0', '1' },
+                    { '1', '1', '0', '1', '0', '0' },
+                    { '0', '0', '1', '0', '1', '1' },
+                    { '1', '0', '0', '1', '1', '0' },
+                    { '0', '1', '0', '1', '0', '1' },
+                    { '0', '1', '1', '0', '1', '0' }
+
+                };
+
+            BinaryPuzzle module = new BinaryPuzzle(grid, io);
+
+            char[,] output = module.Solve();
+
+            Assert.IsTrue(SameGrid(answer, output));
+
+            io.Close();
+        }
+
+        [TestMethod]
+        public void Test3()
+        {
+            char[,] grid = new char[,]
+                {
+                    { '-', '1', '-', '-', '-', '-' },
+                    { '-', '1', '-', '-', '1', '1' },
+                    { '-', '-', '0', '-', '-', '0' },
+                    { '-', '-', '-', '-', '-', '-' },
+                    { '0', '-', '-', '-', '-', '-' },
+                    { '-', '-', '-', '-', '-', '-' }
+
+                };
+
+
+            char[,] answer = new char[,]
+                {
+                    { '0', '1', '1', '0', '0', '1' },
+                    { '0', '1', '0', '0', '1', '1' },
+                    { '1', '0', '0', '1', '1', '0' },
+                    { '1', '0', '1', '0', '0', '1' },
+                    { '0', '1', '0', '1', '1', '0' },
+                    { '1', '0', '1', '1', '0', '0' }
+
+                };
+
+            BinaryPuzzle module = new BinaryPuzzle(grid, io);
+
+            char[,] output = module.Solve();
+
+            Assert.IsTrue(SameGrid(answer, output));
+
+            io.Close();
+        }
+
+        [TestMethod]
+        public void Test4()
+        {
+            char[,] grid = new char[,]
+                {
+                    { '-', '-', '0', '-', '-', '-' },
+                    { '1', '1', '-', '0', '-', '-' },
+                    { '-', '-', '-', '-', '-', '-' },
+                    { '-', '-', '-', '-', '-', '-' },
+                    { '-', '-', '-', '-', '0', '-' },
+                    { '-', '-', '-', '1', '-', '1' }
+
+                };
+
+
+            char[,] answer = new char[,]
+                {
+                    { '0', '1', '0', '0', '1', '1' },
+                    { '1', '1', '0', '0', '1', '0' },
+                    { '1', '0', '1', '1', '0', '0' },
+                    { '0', '0', '1', '0', '1', '1' },
+                    { '1', '1', '0', '1', '0', '0' },
+                    { '0', '0', '1', '1', '0', '1' }
+
+                };
+
+            BinaryPuzzle module = new BinaryPuzzle(grid, io);
+
+            char[,] output = module.Solve();
+
+            Assert.IsTrue(SameGrid(answer, output));
+
+            io.Close();
+        }
+
+        [TestMethod]
+        public void Test5()
+        {
+            char[,] grid = new char[,]
+                {
+                    { '-', '0', '-', '-', '-', '1' },
+                    { '-', '-', '-', '-', '-', '-' },
+                    { '-', '-', '1', '-', '-', '-' },
+                    { '-', '0', '-', '0', '-', '-' },
+                    { '-', '0', '-', '-', '-', '1' },
+                    { '-', '-', '-', '0', '-', '-' }
+
+                };
+
+
+            char[,] answer = new char[,]
+                {
+                    { '0', '0', '1', '1', '0', '1' },
+                    { '1', '1', '0', '1', '0', '0' },
+                    { '0', '1', '1', '0', '1', '0' },
+                    { '0', '0', '1', '0', '1', '1' },
+                    { '1', '0', '0', '1', '0', '1' },
+                    { '1', '1', '0', '0', '1', '0' }
+
+                };
+
+            BinaryPuzzle module = new BinaryPuzzle(grid, io);
+
+            char[,] output = module.Solve();
+
+            Assert.IsTrue(SameGrid(answer, output));
+
+            io.Close();
+        }
+
+        private bool SameGrid(char[,] b1, char[,] b2)
+        {
+            int rowLength = b1.GetLength(0);
+            int colLength = b1.GetLength(1);
+
+            for (int row = 0; row < rowLength; row++)
+            {
+                for (int col = 0; col < colLength; col++)
+                {
+                    if (b1[row, col] != b2[row, col])
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+
     }
 }
