63575dd1f18821a4815fa27ea19ad5e4b0e5e732
Nya <nyabentley@gmail.com>
2022-12-21 16:16:33 -0500

    Created tests for Adventure Game

--- AdventureGameTest.cs
+++ AdventureGameTest.cs
@@ -6,71 +6,41 @@ using System.Windows.Forms;
 namespace ModuleTest
 {
     [TestClass]
-    public class _3DMazeTest
+    public class AdventureGameTest
     {
-        //https://ktane.timwi.de/More/Logfile%20Analyzer.html#file=6eccc2452db5b8806fd0106803d0d5885979cb24;bomb=GQ2EI1
+        //https://ktane.timwi.de/lfa#file=90f0a6d56e9477ae97f1e17ffea79de49dac6390
 
         StreamWriter io = new StreamWriter("dummy.txt");
 
 
-        Bomb bomb = new Bomb(Day.Sunday, "GQ2EI1", 0, 0,
+        Bomb bomb = new Bomb(Day.Sunday, "A39QH2", 2, 1,
             new Indicator("BOB", false, false), new Indicator("CAR", false, false), new Indicator("CLR", false, false),
-            new Indicator("FRK", false, false), new Indicator("FRQ", false, false), new Indicator("IND", false, false),
-            new Indicator("MSA", false, false), new Indicator("NSA", false, false), new Indicator("SIG", true, true),
+            new Indicator("FRK", false, false), new Indicator("FRQ", true, false), new Indicator("IND", true, true),
+            new Indicator("MSA", false, false), new Indicator("NSA", false, false), new Indicator("SIG", false, false),
             new Indicator("SND", false, false), new Indicator("TRN", false, false), new List<Plate>()
         {
-            new Plate(false, false, false, false, false, false),
-            new Plate(false, true, false, true, false, true) });
+            new Plate(false, false, true, false, false, false),
+            new Plate(true, false, false, false, false, false) });
 
         [TestMethod]
         public void Test1()
         {
-            _3DMaze module = new _3DMaze(bomb, io);
+            AdventureGame.Item item1 = AdventureGame.Item.BELLOWS;
+            AdventureGame.Item item2 = AdventureGame.Item.SMALLDOG;
+            AdventureGame.Item item3 = AdventureGame.Item.LAMP;
+            AdventureGame.Item item4 = AdventureGame.Item.FEATHER;
+            AdventureGame.Item item5 = AdventureGame.Item.STEPLADDER;
 
-            module.SetMaze("ADH");
+            AdventureGame module = new AdventureGame(9, 8, 1, 67, 23, 10, 95, 
+                                                    "NASTY KNIFE", "LONGBOW", " MAGIC ORB", 
+                                                    item1, item2, item3, item4, item5, 
+                                                    "LIZARD", bomb, io);
 
-            //finds correct cardinal space
+            string[] answer = module.Solve(true).Split('\n');
 
-            module.PlayerPosition = module.Maze[0, 0];
-
-            //compatile coordinates: [0,5] [2,1] [5,0]
-
-            module.FindCardinal();
-
-            _3DMaze.Node playerPos = module.PlayerPosition;
-
-            bool validCardinalSpace = (playerPos.Row == 0 && playerPos.Colunm == 5) ||
-                                      (playerPos.Row == 2 && playerPos.Colunm == 1) ||
-                                      (playerPos.Row == 5 && playerPos.Colunm == 0);
-
-            Assert.IsTrue(validCardinalSpace);
-
-            //finds correct wall to run into
-
-            module.MainCardinalGoal = "NORTH";
-
-            int row = module.FindRow();
-
-            int column = module.FindColumn();
-
-            module.MainGoal = module.Maze[row, column];
-
-            Assert.AreEqual(2, row);
-            Assert.AreEqual(2, column);
-
-            Assert.AreEqual(module.Maze[row, column], module.MainGoal);
-
-            module.UpdateGoal();
-
-
-
-            //-MainCardinalGoal is correct
-
-            Assert.AreEqual(module.Maze[0,2], module.MainGoal);
-
-            //-SecondaryCardinalGoal is correct
-
-            Assert.AreEqual(module.Maze[7, 2], module.SecondaryGoal);
+            Assert.IsTrue(answer.Contains("LONGBOW"));
+            Assert.IsTrue(answer.Contains(item2.ToString()));
+            Assert.IsTrue(answer.Contains(item4.ToString()));
 
             io.Close();
         }
@@ -78,51 +48,21 @@ namespace ModuleTest
         [TestMethod]
         public void Test2()
         {
-            _3DMaze module = new _3DMaze(bomb, io);
+            AdventureGame.Item item1 = AdventureGame.Item.BALLOON;
+            AdventureGame.Item item2 = AdventureGame.Item.CHEATCODE;
+            AdventureGame.Item item3 = AdventureGame.Item.STEPLADDER;
+            AdventureGame.Item item4 = AdventureGame.Item.MOONSTONE;
+            AdventureGame.Item item5 = AdventureGame.Item.LAMP;
 
-            module.SetMaze("ABH");
+            AdventureGame module = new AdventureGame(10, 9, 10, 41, 43, 10.4, 93,
+                                                    "NASTY KNIFE", "LONGBOW", "GRIMOIRE",
+                                                    item1, item2, item3, item4, item5,
+                                                    "TROLL", bomb, io);
 
-            //finds correct cardinal space
+            string[] answer = module.Solve(true).Split('\n');
 
-            module.PlayerPosition = module.Maze[0, 0];
-
-            //compatile coordinates: [1,0] [3,4] [6,5]
-
-            module.FindCardinal();
-
-            _3DMaze.Node playerPos = module.PlayerPosition;
-
-            bool validCardinalSpace = (playerPos.Row == 1 && playerPos.Colunm == 0) ||
-                                      (playerPos.Row == 3 && playerPos.Colunm == 4) ||
-                                      (playerPos.Row == 6 && playerPos.Colunm == 5);
-
-            Assert.IsTrue(validCardinalSpace);
-
-            //finds correct wall to run into
-
-            module.MainCardinalGoal = "SOUTH";
-
-            int row = module.FindRow();
-
-            int column = module.FindColumn();
-
-            module.MainGoal = module.Maze[row, column];
-
-            Assert.AreEqual(2, row);
-            Assert.AreEqual(2, column);
-
-
-            Assert.AreEqual(module.Maze[row, column], module.MainGoal);
-
-            module.UpdateGoal();
-
-            //-MainCardinalGoal is correct
-
-            Assert.AreEqual(module.Maze[4, 2], module.MainGoal);
-
-            //-SecondaryCardinalGoal is correct
-
-            Assert.AreEqual(module.Maze[5, 2], module.SecondaryGoal);
+            Assert.IsTrue(answer.Contains("GRIMOIRE"));
+            Assert.IsTrue(answer.Contains(item3.ToString()));
 
             io.Close();
         }
@@ -130,51 +70,23 @@ namespace ModuleTest
         [TestMethod]
         public void Test3()
         {
-            _3DMaze module = new _3DMaze(bomb, io);
+            AdventureGame.Item item1 = AdventureGame.Item.SMALLDOG;
+            AdventureGame.Item item2 = AdventureGame.Item.BELLOWS;
+            AdventureGame.Item item3 = AdventureGame.Item.BALLOON;
+            AdventureGame.Item item4 = AdventureGame.Item.FEATHER;
+            AdventureGame.Item item5 = AdventureGame.Item.SYMBOL;
 
-            module.SetMaze("CDH");
+            AdventureGame module = new AdventureGame(7, 6, 3, 50, 45, 10.2, 104,
+                                                    "CABER", "BROADSWORD", "GRIMOIRE",
+                                                    item1, item2, item3, item4, item5,
+                                                    "LIZARD", bomb, io);
 
-            //finds correct cardinal space
+            string[] answer = module.Solve(true).Split('\n');
 
-            module.PlayerPosition = module.Maze[0, 0];
-
-            //compatile coordinates: [1,5] [6,0] [6,6]
-
-            module.FindCardinal();
-
-            _3DMaze.Node playerPos = module.PlayerPosition;
-
-            bool validCardinalSpace = (playerPos.Row == 1 && playerPos.Colunm == 5) ||
-                                      (playerPos.Row == 6 && playerPos.Colunm == 0) ||
-                                      (playerPos.Row == 6 && playerPos.Colunm == 6);
-
-            Assert.IsTrue(validCardinalSpace);
-
-            //finds correct wall to run into
-
-            module.MainCardinalGoal = "SOUTH";
-
-            int row = module.FindRow();
-
-            int column = module.FindColumn();
-
-            module.MainGoal = module.Maze[row, column];
-
-            Assert.AreEqual(2, row);
-            Assert.AreEqual(2, column);
-
-
-            Assert.AreEqual(module.Maze[row, column], module.MainGoal);
-
-            module.UpdateGoal();
-
-            //-MainCardinalGoal is correct
-
-            Assert.AreEqual(module.Maze[4, 2], module.MainGoal);
-
-            //-SecondaryCardinalGoal is correct
-
-            Assert.AreEqual(module.Maze[5, 2], module.SecondaryGoal);
+            Assert.IsTrue(answer.Contains("CABER"));
+            Assert.IsTrue(answer.Contains(item1.ToString()));
+            Assert.IsTrue(answer.Contains(item4.ToString()));
+            Assert.IsTrue(answer.Contains(item5.ToString()));
 
             io.Close();
         }
@@ -182,51 +94,21 @@ namespace ModuleTest
         [TestMethod]
         public void Test4()
         {
-            _3DMaze module = new _3DMaze(bomb, io);
+            AdventureGame.Item item1 = AdventureGame.Item.SYMBOL;
+            AdventureGame.Item item2 = AdventureGame.Item.SMALLDOG;
+            AdventureGame.Item item3 = AdventureGame.Item.CHEATCODE;
+            AdventureGame.Item item4 = AdventureGame.Item.BATTERY;
+            AdventureGame.Item item5 = AdventureGame.Item.POTION;
 
-            module.SetMaze("BCD");
+            AdventureGame module = new AdventureGame(1, 10, 3, 53, 0, 10.4, 100,
+                                                    "NASTY KNIFE", "CABER", "BROADSWORD",
+                                                    item1, item2, item3, item4, item5,
+                                                    "EAGLE", bomb, io);
 
-            //finds correct cardinal space
+            string[] answer = module.Solve(true).Split('\n');
 
-            module.PlayerPosition = module.Maze[0, 0];
-
-            //compatile coordinates: [1,6] [2,1] [6,4]
-
-            module.FindCardinal();
-
-            _3DMaze.Node playerPos = module.PlayerPosition;
-
-            bool validCardinalSpace = (playerPos.Row == 1 && playerPos.Colunm == 6) ||
-                                      (playerPos.Row == 2 && playerPos.Colunm == 1) ||
-                                      (playerPos.Row == 6 && playerPos.Colunm == 4);
-
-            Assert.IsTrue(validCardinalSpace);
-
-            //finds correct wall to run into
-
-            module.MainCardinalGoal = "EAST";
-
-            int row = module.FindRow();
-
-            int column = module.FindColumn();
-
-            module.MainGoal = module.Maze[row, column];
-
-            Assert.AreEqual(2, row);
-            Assert.AreEqual(2, column);
-
-
-            Assert.AreEqual(module.Maze[row, column], module.MainGoal);
-
-            module.UpdateGoal();
-
-            //-MainCardinalGoal is correct
-
-            Assert.AreEqual(module.Maze[2, 3], module.MainGoal);
-
-            //-SecondaryCardinalGoal is correct
-
-            Assert.AreEqual(module.Maze[2, 4], module.SecondaryGoal);
+            Assert.IsTrue(answer.Contains("NASTY KNIFE"));
+            Assert.IsTrue(answer.Contains(item2.ToString()));
 
             io.Close();
         }
@@ -234,51 +116,23 @@ namespace ModuleTest
         [TestMethod]
         public void Test5()
         {
-            _3DMaze module = new _3DMaze(bomb, io);
+            AdventureGame.Item item1 = AdventureGame.Item.CRYSTALBALL;
+            AdventureGame.Item item2 = AdventureGame.Item.HARDDRIVE;
+            AdventureGame.Item item3 = AdventureGame.Item.LAMP;
+            AdventureGame.Item item4 = AdventureGame.Item.BELLOWS;
+            AdventureGame.Item item5 = AdventureGame.Item.FEATHER;
 
-            module.SetMaze("ADH");
+            AdventureGame module = new AdventureGame(1, 2, 8, 44, 8, 9.7, 107,
+                                                    "MAGIC ORB", "LONGBOW", "GRIMOIRE",
+                                                    item1, item2, item3, item4, item5,
+                                                    "DEMON", bomb, io);
 
-            //finds correct cardinal space
+            string[] answer = module.Solve(true).Split('\n');
 
-            module.PlayerPosition = module.Maze[0, 0];
-
-            //compatile coordinates: [0,5] [2,1] [5,0]
-
-            module.FindCardinal();
-
-            _3DMaze.Node playerPos = module.PlayerPosition;
-
-            bool validCardinalSpace = (playerPos.Row == 0 && playerPos.Colunm == 5) ||
-                                      (playerPos.Row == 2 && playerPos.Colunm == 1) ||
-                                      (playerPos.Row == 5 && playerPos.Colunm == 0);
-
-            Assert.IsTrue(validCardinalSpace);
-
-            //finds correct wall to run into
-
-            module.MainCardinalGoal = "NORTH";
-
-            int row = module.FindRow();
-
-            int column = module.FindColumn();
-
-            module.MainGoal = module.Maze[row, column];
-
-            Assert.AreEqual(2, row);
-            Assert.AreEqual(2, column);
-
-
-            Assert.AreEqual(module.Maze[row, column], module.MainGoal);
-
-            module.UpdateGoal();
-
-            //-MainCardinalGoal is correct
-
-            Assert.AreEqual(module.Maze[0, 2], module.MainGoal);
-
-            //-SecondaryCardinalGoal is correct
-
-            Assert.AreEqual(module.Maze[7, 2], module.SecondaryGoal);
+            Assert.IsTrue(answer.Contains("GRIMOIRE"));
+            Assert.IsTrue(answer.Contains(item1.ToString()));
+            Assert.IsTrue(answer.Contains(item3.ToString()));
+            Assert.IsTrue(answer.Contains(item5.ToString()));
 
             io.Close();
         }
