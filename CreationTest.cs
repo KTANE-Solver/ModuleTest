111b7f3995b279ee12208933fd88d12a559dd050
Nya <nyabentley@gmail.com>
2023-03-13 19:43:09 -0400

    Uploaded creation test

---
+++ CreationTest.cs
@@ -0,0 +1,21 @@
+using System.Drawing;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using New_KTANE_Solver;
+using System.Windows.Forms;
+using System.Net.NetworkInformation;
+
+namespace ModuleTest
+{
+    [TestClass]
+    public class CreationTest
+    {
+        //https://ktane.timwi.de/More/Logfile%20Analyzer.html#file=e8f6e6a9015cb8174b91f87738246b32893d80d8;bomb=QB4IU3
+        StreamWriter io = new StreamWriter("dummy.txt");
+
+        [TestMethod]
+        public void Test1()
+        {
+            
+        }
+    }
+}
